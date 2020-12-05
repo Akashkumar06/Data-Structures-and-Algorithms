88c75508ded93840c7af5f79ee914b2a63d02f18
Akash Kumar <akashrajput2012001@gmail.com>
2020-12-05 09:38:49 +0530

    ItemCountAdded Now

---
+++ 30 days Of Compatative Programming/Day 3/ToPointerTheorem.c
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+bool Sum(int a[],int n,int x){
+    int start=i;
+    int end=n-1;
+    while(start<end){
+
+        if(a[start]+a[end]==x) return true;
+        else if(a[start]+a[end]>x) end--;
+        else start++;
+    }
+
+
+
+}
+int main(){
+int a[10];
+int n=sizeof(a)/sizeof(a[0]);
+for(int i=0;i<)
+
+
+}
