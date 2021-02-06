951e4c7ba7ad745cd1852db2ce3c3b5b5197b584
Akash Kumar <akashrajput2012001@gmail.com>
2021-02-06 23:32:18 +0530

    more questions

--- Maths/factorial.cpp
+++ Maths/factorial.cpp
@@ -2,40 +2,17 @@
 #define MAX 500
 using namespace std;
 
-int multiply(int x,int res[],int n){
-    int c=0;
 
-    for(int i=0;i<n;i++){
-        int prod=res[i]*x+c;
-        res[i]=prod%10;
-        c=prod/10;
-
-
-
-    }
-    while(c){
-        res[n]=c%10;
-        c=c/10;
-        n++;
-
-
-    }
-    return n;
-
-
-
-
-
-}
-void factorial(int val){
-    int res[MAX];
+string factorial(int val){
+    int res[160];
+    string str;
     res[0]=1;
     int n=1;
     for(int k=2;k<=val;k++)
         n=multiply(k,res,n);
-    cout<<"Factorial of Given number is :"<<endl;
+
     for(int i=n-1;i>=0;i--)
-        cout<<res[i];
+        str.push_back(res[i]);
 
 
 
