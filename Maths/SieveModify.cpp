b3c4e0067faad3cf612849116bfdb0f9da51f736
Akash Kumar <akashrajput2012001@gmail.com>
2021-04-21 00:18:34 +0530

    pacalTriangle

--- Maths/SieveModify.cpp
+++ Maths/SieveModify.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 const long long MAX_SIZE = 1000001;
@@ -8,33 +7,25 @@ vector<long long >SPF(MAX_SIZE);
 
 void manipulated_seive(int N)
 {
+    isprime[0]=isprime[1]=false;
 
-	isprime[0] = isprime[1] = false ;
-
-	for (long long int i=2; i<N ; i++)
-	{
-
-		if (isprime[i])
-		{
-
-			prime.push_back(i);
-
-			SPF[i] = i;
-		}
-
-		for (long long int j=0;
-			j < (int)prime.size() &&
-			i*prime[j] < N && prime[j] <= SPF[i];
-			j++)
-		{
-			isprime[i*prime[j]]=false;
+    for(int i=2;i<N;i++){
+        if(isprime[i]){
+                prime.push_back(i);
+                SPF[i]=i;
 
 
-			SPF[i*prime[j]] = prime[j] ;
-		}
-	}
+        }
+            for(int j=0;i<prime.size() && i*prime[j]<=N && prime[i]<SPF[i];j++){
+
+            isprime[i*prime[j]]=false;
+            SPF[i*prime[j]]=prime[j];
+
+
 }
+    }
 
+}
 
 int main()
 {
